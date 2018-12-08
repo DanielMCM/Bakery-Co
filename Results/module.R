1dd2146a91ea739db1bd90bab5532c9f909494aa
Javier de la Rúa Martínez <javierdlrm@outlook.com>
2018-12-08 17:23:43 +0100

    Add verbatim with rules and quality

--- Results/module.R
+++ Results/module.R
@@ -24,7 +24,7 @@ results_ui <- function(id) {
                         box(title = h5("Support level"), class = "full-width-container", sliderInput(ns("trans.level.support"), NULL, min = 0.005, max = 0.1, value = 0.05)),
                         box(title = h5("Confidence level"), class = "full-width-container", sliderInput(ns("trans.level.confidence"), NULL, min = 0.1, max = 0.9, value = 0.8, step = 0.1))),
                     fluidRow(
-                        box(width = 12, h5("Rules")),
+                        box(width = 12, h5("Rules"), verbatimTextOutput(ns("trans.rules"))),
                         box(plotOutput(ns("trans.plot.rules_graph"))),
                         box(plotOutput(ns("trans.plot.rules_circle"))),
                         box(width = 12, br()),
@@ -47,7 +47,7 @@ results_ui <- function(id) {
                         box(title = h5("Support level"), class = "full-width-container", sliderInput(ns("trans1.level.support"), NULL, min = 0.005, max = 0.1, value = 0.05)),
                         box(title = h5("Confidence level"), class = "full-width-container", sliderInput(ns("trans1.level.confidence"), NULL, min = 0.1, max = 0.9, value = 0.8, step = 0.1))),
                     fluidRow(
-                        box(width = 12, h5("Rules")),
+                        box(width = 12, h5("Rules"), verbatimTextOutput(ns("trans1.rules"))),
                         box(plotOutput(ns("trans1.plot.rules_graph"))),
                         box(plotOutput(ns("trans1.plot.rules_circle"))),
                         box(width = 12, br()),
@@ -65,7 +65,7 @@ results_ui <- function(id) {
                         box(title = h5("Support level"), class = "full-width-container", sliderInput(ns("trans2.level.support"), NULL, min = 0.005, max = 0.1, value = 0.05)),
                         box(title = h5("Confidence level"), class = "full-width-container", sliderInput(ns("trans2.level.confidence"), NULL, min = 0.1, max = 0.9, value = 0.8, step = 0.1))),
                     fluidRow(
-                        box(width = 12, h5("Rules")),
+                        box(width = 12, h5("Rules"), verbatimTextOutput(ns("trans2.rules"))),
                         box(plotOutput(ns("trans2.plot.rules_graph"))),
                         box(plotOutput(ns("trans2.plot.rules_circle"))),
                         box(width = 12, br()),
@@ -114,10 +114,19 @@ results_server <- function(input, output, session) {
     # Trans
 
     trans.rules <- reactive({
+        req(data.trans)
         rules <- apriori(data.trans(), parameter = list(sup = input$trans.level.support, conf = input$trans.level.confidence, target = "rules"))
     })
 
+    output$trans.rules <- renderPrint({
+        req(trans.rules)
+        trans.rules.df <- data.frame(lhs = labels(trans.rules()@lhs), rhs = labels(trans.rules()@rhs), trans.rules()@quality)
+        return(trans.rules.df[order(-trans.rules.df$lift),])
+    })
+
     output$trans.plot.linear <- renderPlot({
+        req(data.trans)
+
         result <- model.calculate_num_rules(data.trans())
 
         # Number of rules found with a support level of 10%, 5%, 1% and 0.5%
@@ -145,37 +154,52 @@ results_server <- function(input, output, session) {
     })
 
     output$trans.summary <- renderPrint({
+        req(data.trans)
         summary(data.trans())
     })
 
     output$trans.glimpse <- renderPrint({
+        req(data.trans)
         glimpse(data.trans())
     })
 
     output$trans.frequency.plot <- renderPlot({
+        req(data.trans)
         itemFrequencyPlot(data.trans(), topN = 15, type = "relative", col = "lightcyan2", xlab = "Item name",
                   ylab = "Frequency (relative)", main = "Relative Item Frequency Plot")
     })
 
     output$trans.plot.rules_graph <- renderPlot({
+        req(trans.rules)
         plot(trans.rules(), method = "graph")
     })
 
     output$trans.plot.rules_circle <- renderPlot({
+        req(trans.rules)
         plot(trans.rules(), method = "graph", control = list(layout = igraph::in_circle()))
     })
 
     output$trans.plot.rules_scatter <- renderPlot({
+        req(trans.rules)
         plot(trans.rules(), measure = c("support", "lift"), shading = "confidence", jitter = 0)
     })
 
     # Trans 1
 
     trans1.rules <- reactive({
+        req(data.trans1)
         rules <- apriori(data.trans1(), parameter = list(sup = input$trans1.level.support, conf = input$trans1.level.confidence, target = "rules"))
     })
 
+    output$trans1.rules <- renderPrint({
+        req(trans1.rules)
+        trans1.rules.df <- data.frame(lhs = labels(trans1.rules()@lhs), rhs = labels(trans1.rules()@rhs), trans1.rules()@quality)
+        return(trans1.rules.df[order(-trans1.rules.df$lift),])
+    })
+
     output$trans1.plot.linear <- renderPlot({
+        req(data.trans1)
+
         result <- model.calculate_num_rules(data.trans1())
 
         # Number of rules found with a support level of 10%, 5%, 1% and 0.5%
@@ -203,37 +227,52 @@ results_server <- function(input, output, session) {
     })
 
     output$trans1.summary <- renderPrint({
+        req(data.trans1)
         summary(data.trans1())
     })
 
     output$trans1.glimpse <- renderPrint({
+        req(data.trans1)
         glimpse(data.trans1())
     })
 
     output$trans1.frequency.plot <- renderPlot({
+        req(data.trans1)
         itemFrequencyPlot(data.trans1(), topN = 15, type = "relative", col = "lightcyan2", xlab = "Item name",
                   ylab = "Frequency (relative)", main = "Relative Item Frequency Plot")
     })
 
     output$trans1.plot.rules_graph <- renderPlot({
+        req(trans1.rules)
         plot(trans1.rules(), method = "graph")
     })
 
     output$trans1.plot.rules_circle <- renderPlot({
+        req(trans1.rules)
         plot(trans1.rules(), method = "graph", control = list(layout = igraph::in_circle()))
     })
 
     output$trans1.plot.rules_scatter <- renderPlot({
+        req(trans1.rules)
         plot(trans1.rules(), measure = c("support", "lift"), shading = "confidence", jitter = 0)
     })
 
     # Trans 2
 
     trans2.rules <- reactive({
+        req(data.trans2)
         rules <- apriori(data.trans2(), parameter = list(sup = input$trans2.level.support, conf = input$trans2.level.confidence, target = "rules"))
     })
 
+    output$trans2.rules <- renderPrint({
+        req(trans2.rules)
+        trans2.rules.df <- data.frame(lhs = labels(trans2.rules()@lhs), rhs = labels(trans2.rules()@rhs), trans2.rules()@quality)
+        return(trans2.rules.df[order(-trans2.rules.df$lift),])
+    })
+
     output$trans2.plot.linear <- renderPlot({
+        req(data.trans2)
+
         result <- model.calculate_num_rules(data.trans2())
 
         # Number of rules found with a support level of 10%, 5%, 1% and 0.5%
@@ -261,27 +300,33 @@ results_server <- function(input, output, session) {
     })
 
     output$trans2.summary <- renderPrint({
+        req(data.trans2)
         summary(data.trans2())
     })
 
     output$trans2.glimpse <- renderPrint({
+        req(data.trans2)
         glimpse(data.trans2())
     })
 
     output$trans2.frequency.plot <- renderPlot({
+        req(data.trans2)
         itemFrequencyPlot(data.trans2(), topN = 15, type = "relative", col = "lightcyan2", xlab = "Item name",
                       ylab = "Frequency (relative)", main = "Relative Item Frequency Plot")
     })
 
     output$trans2.plot.rules_graph <- renderPlot({
+        req(trans2.rules)
         plot(trans2.rules(), method = "graph")
     })
 
     output$trans2.plot.rules_circle <- renderPlot({
+        req(trans2.rules)
         plot(trans2.rules(), method = "graph", control = list(layout = igraph::in_circle()))
     })
 
     output$trans2.plot.rules_scatter <- renderPlot({
+        req(trans2.rules)
         plot(trans2.rules(), measure = c("support", "lift"), shading = "confidence", jitter = 0)
     })
 
